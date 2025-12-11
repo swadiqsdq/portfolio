1853381acc2db880f5fa9c5fc218c1c7e1657286
swadiqsdq <165919348+swadiqsdq@users.noreply.github.com>
2025-12-11 22:24:17 +0530

    Change admin URL path to 'adminsettings/'

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminsettings/', admin.site.urls),
     path("",include('app.urls')),
     path('account/', include('account.urls')),
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
