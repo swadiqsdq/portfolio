acce2869a6669324b51ab38fec9dfdc5c9bec32e
muhamedswadiq <swadiqsdq@gmail.com>
2025-12-14 01:27:10 +0530

    media root

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('adminsettings/', admin.site.urls),
     path("",include('app.urls')),
     path('account/', include('account.urls')),
-]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+    # Serve media files via a view (works even with DEBUG=False for testing/small projects)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+# Optional: for DEBUG=True, static() works automatically
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
